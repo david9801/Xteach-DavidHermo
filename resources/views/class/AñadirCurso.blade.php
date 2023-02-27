42ae9f374f2afee7a51fe1f18d49ae1d98218694
David Hermo Blanco <davidhermo.tei@gmail.com>
2023-02-27 23:33:08 +0100

    added dropzone

--- resources/views/class/AñadirCurso.blade.php
+++ resources/views/class/AñadirCurso.blade.php
@@ -26,6 +26,23 @@
 
    </style>
 
+    <script>
+        //para arrastrar y soltar con dropzone
+        Dropzone.options.myAwesomeDropzone = {
+            paramName: "archivo",
+            maxFilesize: 10, // en MB
+            acceptedFiles: ".ppt,.pptx",
+            dictDefaultMessage: "Por favor!Arrastra y suelta tu archivo aquí",
+            dictFallbackMessage: "Tu navegador no admite la carga de archivos por arrastrar y soltar", // Mensaje si el navegador no admite esta funcionalidad
+            dictInvalidFileType: "No puedes subir este archivo. Solo Power Point)", // Mensaje si el archivo tiene una extensión no permitida
+            dictResponseError: "Ha ocurrido un error al subir el archivo",
+            dictCancelUpload: "Cancelar subida",
+            dictCancelUploadConfirmation: "¿Estás seguro de que deseas cancelar la subida?",
+            dictRemoveFile: "Eliminar archivo",
+            dictMaxFilesExceeded: "Solo puedes subir un archivo"
+        };
+    </script>
+
     <div id="div-welcome">
         <div class="container">
             <div class="row justify-content-center">
@@ -35,7 +52,7 @@
                             <h2 class="text-center">¡Añade el curso que quieras impartir!</h2>
                         </div>
                         <div class="card-body text-center">
-                            <form action="{{ route('createCurso') }}" method="post" enctype="multipart/form-data">
+                            <form action="{{ route('createCurso') }}" method="post" enctype="multipart/form-data" class="dropzone" id="my-awesome-dropzone">
                                 @csrf
                                 <div class="mb-3" id="input">
                                     <label for="name" class="form-label">Nombre del Curso</label>
@@ -57,6 +74,7 @@
                 </div>
             </div>
         </div>
+
 
         @if ($errors->any())
             <div class="alert alert-danger d-flex justify-content-center">
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -13,7 +13,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
-
+    <!--Añadimos dropzone para que en un form si queremos subir algo se pueda arrastrar/soltar  -->
+    <!-- estilos CSS de Dropzone -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.9.3/dropzone.min.css" integrity="sha512-tKcMsN1UmnR6+RzlMu42XZ6KjIz7C/vTupTfzjAAWw8JY18BQlGm13sZncXWZs8aFFtjN/WQplwvl+8aSFDgjg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!--script de Dropzone -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.9.3/min/dropzone.min.js" integrity="sha512-tJ8Nx6xjE6IpkbQT1DGMJOp8jzwP04+TmyTb05egEwWsd8JzrBdnG0ndTTew2lWZj8vpvubXNpTlmHTbEJwK8w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <!-- Icons fontawesome  -->
     <script src="https://kit.fontawesome.com/06c6f7ab73.js" crossorigin="anonymous"></script>
     <!-- Icons Bootstrap  -->
